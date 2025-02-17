5ed3f0a9b0557e38b208634fce6bf5a67a6f4516
toanpm <thanhyasua1@gmail.com>
2025-02-18 03:54:21 +0700

    add speed insight

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import ErrorBoundary from './ErrorBoundary';
 import AppCanvas from './components/AppCanvas';
 import Toast from './Toast';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 function App() {
   const [toast, setToast] = useState(null);
 
@@ -22,6 +24,7 @@ function App() {
       )}
       <AppCanvas onShowToast={showToast} />
       <Analytics />
+      <SpeedInsights />
     </ErrorBoundary>
   );
 }
